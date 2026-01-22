664c5c7da10663890d2a9438800e12c151b0cc88
ferney.manco@coordiutil.com <ferney.manco@coordiutil.com>
2026-01-22 15:35:22 -0500

    custard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AItghDCi72RK4L6Nutgyhj6yh431b0CMXyhg2X0ws",
+    apiKey: "AIzaSyDCi72RK4L6NutyTS5te1b0CMXFCo2X0ws",
     authDomain: "useless-game.firebaseapp.com",
     projectId: "useless-game",
     storageBucket: "useless-game.firebasestorage.app",
